bb8c7a96f2dfd8ce6e977307aa7236e464843078
rrtxt <rizfiferdian1@gmail.com>
2025-09-26 14:26:07 +0700

    restructure code and add movement bound

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,58 @@
 #include "raylib.h"
+#include "raymath.h"
+#include "iostream"
 
-const int SCREEN_WIDTH = GetScreenWidth();
-const int SCREEN_HEIGHT = GetScreenHeight();
+const int VELOCITY = 5;
+const int TIMESCALE = 1;
 
 class Player{
     public:
         Vector2 position;
         int width;
         int height;
-        Player(){
-            position.x = SCREEN_WIDTH/2;
-            position.y = SCREEN_HEIGHT/2;
+        Player(float x , float y){
+            position.x = x;
+            position.y = y;
             width = 20;
             height = 30;
         }
-};
 
-const int VELOCITY = 5;
-const int TIMESCALE = 1;
-Player player = Player();
+        void Update(){
+            if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
+                position.y -= 1 * VELOCITY * TIMESCALE;
+            }
+            if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
+                position.y += 1 * VELOCITY * TIMESCALE;
+            }
+            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
+                position.x += 1 * VELOCITY * TIMESCALE;
+            }
+            if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
+                position.x -= 1 * VELOCITY * TIMESCALE;
+            }
 
-void InputUpdate() {
-    if (IsKeyDown(KEY_UP)) {
-        player.position.y -= 1 * VELOCITY * TIMESCALE;
-    }
-    if (IsKeyDown(KEY_DOWN)) {
-        player.position.y += 1 * VELOCITY * TIMESCALE;
-    }
-    if (IsKeyDown(KEY_RIGHT)) {
-        player.position.x += 1 * VELOCITY * TIMESCALE;
-    }
-    if (IsKeyDown(KEY_LEFT)) {
-        player.position.x -= 1 * VELOCITY * TIMESCALE;
-    }
-}
+                position.x = Clamp(position.x, 0, GetScreenWidth() - width);
+                position.y = Clamp(position.y, 0, GetScreenHeight() - height);
+            }
 
-void Update(){
-    DrawRectangle(player.position.x, player.position.y, player.width, player.height, RED);
-    InputUpdate();
-}
-
-int main() {
-    InitWindow(800, 600, "Jet Game");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
+            void Draw(){
+                DrawRectangle(position.x, position.y, width, height, RED);
+        }
+    };
+    
+    
+    int main() {
+        InitWindow(600, 900, "Jet Game");
+        SetTargetFPS(60);
+        Player player = Player(GetScreenWidth() / 2, GetScreenHeight() / 2);
+        std::cout << "Initial Player Position : " << player.position.x << ", " << player.position.y << std::endl;
+        
+        while (!WindowShouldClose()) {
+            player.Update();
+            
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        Update();
+        player.Draw();
         EndDrawing();
     }
 
