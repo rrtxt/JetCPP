6b2dae559b6ed8ddbf4f79b0c0ebdfb51dd743de
rrtxt <rizfiferdian1@gmail.com>
2025-09-29 12:49:02 +0700

    add collision check for player and enemy, enemy and bullet

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ const int TIMESCALE = 1;
 class Bullet{
     public:
         Vector2 position;
+        Rectangle collision;
         float speed;
         int width;
         int height;
@@ -18,14 +19,21 @@ class Bullet{
         Bullet(float x, float y){
             position.x = x;
             position.y = y;
-            speed = 350;
+            collision.x = x;
+            collision.y = y;
             width = 5;
             height = 10;
+            collision.width = width;
+            collision.height = height;
+            speed = 350;
         }
         void Update(){
             if(active){
                 // Move upward
                 position.y -= speed * GetFrameTime();
+                
+                // Update collision box
+                collision.y = position.y;
                 
                 // Set inactive if off-screen
                 if(position.y + height < 0){
@@ -46,12 +54,17 @@ class Player{
         Vector2 position;
         Vector2 size;
         Vector2 inputDirection;
+        Rectangle collision;
         std::vector<Bullet> bullets;
         float shootCooldown;
         float fireRate;
         Player(int width, int height, float x , float y){
             position.x = x;
             position.y = y;
+            collision.x = x;
+            collision.y = y;
+            collision.width = width;
+            collision.height = height;
             size.x = width;
             size.y = height;
             shootCooldown = 0;
@@ -104,6 +117,10 @@ class Player{
             Vector2 velocity = Vector2Scale(inputDirection, VELOCITY * GetFrameTime() * TIMESCALE);
             position = Vector2Add(position, velocity);
             position = Vector2Clamp(position, {0, 0}, {GetScreenWidth() - size.x, GetScreenHeight() - size.y});
+
+            // Update collision box
+            collision.x = position.x;
+            collision.y = position.y;
         }
 
         void Draw(){
@@ -114,15 +131,22 @@ class Player{
         }
 };
 
+
+
 class Enemy{
     public:
-        Vector2 position;
-        Vector2 size;
-        float speed;
-        bool active;
-        Enemy(float x, float y){
+    Vector2 position;
+    Vector2 size;
+    Rectangle collision;
+    float speed;
+    bool active;
+    Enemy(float x, float y){
             position.x = x;
             position.y = y;
+            collision.x = x;
+            collision.y = y;
+            collision.width = 30;
+            collision.height = 30;
             size.x = 30;
             size.y = 30;
             speed = 100;
@@ -132,6 +156,9 @@ class Enemy{
             if(active){
                 // Move downward
                 position.y += speed * GetFrameTime();
+
+                // Update collision box
+                collision.y = position.y;
                 
                 // Set inactive if off-screen
                 if(position.y > GetScreenHeight()){
@@ -146,9 +173,9 @@ class Enemy{
             }
         }
 };
-
-class Spawner{
-    public:
+    
+    class Spawner{
+        public:
         Vector2 position;
         float spawnCooldown;
         float spawnRate;
@@ -173,45 +200,76 @@ class Spawner{
                 enemies.push_back(newEnemy);
                 spawnCooldown = spawnRate;
             }
-
+            
             // Update enemies
             for (auto& enemy : enemies) {
                 enemy.Update();
             }
-
+            
             // Remove inactive enemies
             enemies.erase(std::remove_if(enemies.begin(), enemies.end(),
-                [](Enemy& e) { return !e.active; }), enemies.end());   
+            [](Enemy& e) { return !e.active; }), enemies.end());   
         }
         void Draw(){
             for(auto& enemy : enemies){
                 enemy.Draw();
             }
         }
-}; 
-    
-int main() {
-    InitWindow(600, 900, "Jet Game");
-    SetTargetFPS(60);
-    Player player = Player(25, 40, GetScreenWidth() / 2, GetScreenHeight() / 2);
-    Spawner spawner = Spawner(GetScreenWidth() / 2 - 15, -30);
-    std::cout << "Initial Player Position : " << player.position.x << ", " << player.position.y << std::endl;
-    
-    while (!WindowShouldClose()) {
-        player.Update();
-        spawner.Update();
+    };
 
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        player.Draw();
-        spawner.Draw();  
-        EndDrawing();
+    class CollisionSystem{
+        public:
+            static void CheckCollisionPlayerEnemy(Player& player, vector<Enemy>& enemies){
+                for(auto& enemy : enemies){
+                    if(enemy.active && CheckCollisionRecs(player.collision, enemy.collision)){
+                        enemy.active = false;
+                        std::cout << "Collision Detected!" << std::endl;
+                    }
+                }
+            }
+
+            static void CheckCollisionBulletEnemy(vector<Bullet>& bullets, vector<Enemy>& enemies){
+                for(auto& bullet : bullets){
+                    for(auto& enemy : enemies){
+                        if(bullet.active && enemy.active && CheckCollisionRecs(bullet.collision, enemy.collision)){
+                            bullet.active = false;
+                            enemy.active = false;
+                            std::cout << "Bullet hit Enemy!" << std::endl;
+                        }
+                    }
+                }
+            }
+    };
+    
+    int main() {
+        InitWindow(600, 900, "Jet Game");
+        SetTargetFPS(60);
+        Player player = Player(25, 40, GetScreenWidth() / 2, GetScreenHeight() / 2);
+        Spawner spawner = Spawner(GetScreenWidth() / 2 - 15, -30);
+        std::cout << "Initial Player Position : " << player.position.x << ", " << player.position.y << std::endl;
+        
+        while (!WindowShouldClose()) {
+            // Update game state
+            player.Update();
+            spawner.Update();
+
+            // Check collisions
+            CollisionSystem::CheckCollisionPlayerEnemy(player, spawner.enemies);
+            CollisionSystem::CheckCollisionBulletEnemy(player.bullets, spawner.enemies);
+            
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            // Draw game elements
+            player.Draw();
+            spawner.Draw();  
+            EndDrawing();
+        }
+        
+        CloseWindow();
+        return 0;                                                                                                                                                              
     }
-
-    CloseWindow();
-    return 0;                                                                                                                                                              
-}
-
-
-
-
+    
+    
+    
+    
