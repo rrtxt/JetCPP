421b48e62d232615b462322a7412efae8dd5d447
rrtxt <rizfiferdian1@gmail.com>
2025-10-15 15:00:17 +0700

    add gamewace class and move spawn delay logic here

---
+++ include/System/Gamewave.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Common.h"
+#include "Spawner.h"
+
+class Gamewave {
+    public:
+        Gamewave(Spawner* spawner, int totalEnemies, float spawnInterval);
+        void Start();
+        void Update();
+        void Draw();
+        bool getIsCompleted() const { return isCompleted; }
+    private:
+        Spawner* spawner;
+        int totalEnemies;
+        int spawnedEnemies;
+        float spawnInterval;
+        float spawnTimer; 
+        bool isCompleted;
+};
---
+++ src/System/Gamewave.cpp
@@ -0,0 +1,32 @@
+#include "Gamewave.h"
+
+Gamewave::Gamewave(Spawner* spawner, int totalEnemies, float spawnInterval)
+    : spawner(spawner), totalEnemies(totalEnemies), spawnInterval(spawnInterval),
+      spawnedEnemies(0), spawnTimer(0.0f), isCompleted(false) {}
+
+void Gamewave::Start() {
+    spawnedEnemies = 0;
+    spawnTimer = 0.0f;
+    isCompleted = false;
+}
+
+void Gamewave::Update() {
+    if (isCompleted) return;
+
+    spawnTimer += GetFrameTime();
+    if (spawnedEnemies < totalEnemies && spawnTimer >= spawnInterval) {
+        spawner->Spawn();
+        spawnedEnemies++;
+        spawnTimer = 0.0f;
+    }
+
+    spawner->Update();
+
+    if (spawnedEnemies >= totalEnemies && spawner->enemies.empty()) {
+        isCompleted = true;
+    }
+}
+
+void Gamewave::Draw() {
+    spawner->Draw();
+}
